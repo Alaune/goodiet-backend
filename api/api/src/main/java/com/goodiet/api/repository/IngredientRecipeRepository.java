84af4e8af99b656924bad175b0a8beb25fb65804
Sara <81881371+saraga13@users.noreply.github.com>
2021-05-17 22:29:29 +0200

    Delete api/api directory

--- api/api/src/main/java/com/goodiet/api/repository/IngredientRecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.goodiet.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.goodiet.api.domain.IngredientRecipe;
-
-public interface IngredientRecipeRepository extends JpaRepository<IngredientRecipe, Long>{
-
-}
--- api/api/src/main/java/com/goodiet/api/repository/MeasureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.goodiet.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.goodiet.api.domain.Measure;
-
-public interface MeasureRepository extends JpaRepository<Measure, Long> {
-
-}
